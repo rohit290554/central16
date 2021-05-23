a78c56ca4fd983f01b6645f408030a12b8c56245
Rohit Saxena <rohitsaxena722@gmail.com>
2021-05-23 15:11:54 +0530

    db connection test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+import mysql.connector as mysql
 
 app = Flask(__name__)
+HOST = 'sql6.freesqldatabase.com'
+USER = 'sql6412984'
+PW = 'EUxK6p6ciM'
+DB = 'sql6412984'
+
+engine = mysql.connect(host=HOST, database=DB, user=USER, password=PW)
+cur = engine.cursor()
 
 
 @app.route("/")
 def home_view():
-    return "<h1>Hello API Test via Heroku deploy</h1>"
+    cur.execute("SELECT * FROM test")
+    rows = cur.fetchall()
+    return jsonify(rows)
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+
+    if request.method == 'POST':
+        name = request.form['name']
+        mob = request.form['mob']
+        cur.execute(''' INSERT INTO test(name, mob) VALUES(%s,%s)''', (name, mob))
+        engine.commit()
+        cur.close()
+        return f"Done!!"
